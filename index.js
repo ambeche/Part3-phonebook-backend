453fd108115e6967d2d1e03f3b9beb66192357cd
Tamanji Che <cheambetamanji@gmail.com>
2020-06-14 23:06:35 +0300

    exer 3.4: route for getting entry by id parameter

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
 const express = require("express");
 const app = express();
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
+let persons = [{
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456",
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+    },
 ];
 
 app.get("/info", (req, res) => {
-  res.send(
-    `<p>Phonebook has info for ${persons.length} people</p> 
+    res.send(
+        `<p>Phonebook has info for ${persons.length} people</p> 
     <p>${new Date()}</p>`
-  );
+    );
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+    res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find((p) => p.id === id);
+
+    person ? res.json(person) : res.status(404).send(`Resource ${id} not found`);
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter( p => p.id !== id)
-    console.log('deleted person', id );
-    
+    const id = Number(req.params.id);
+    persons = persons.filter((p) => p.id !== id);
+    console.log("deleted person", id);
+
     res.status(204).end();
-  });
+});
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
