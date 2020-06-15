20575d2f232393e1d8d1c4a3247b12d4a24843e9
Tamanji Che <cheambetamanji@gmail.com>
2020-06-15 14:26:10 +0300

    exer 3.5-3.6: route for creating entries and error handling implementations

--- index.js
+++ index.js
@@ -1,52 +1,82 @@
 const express = require("express");
 const app = express();
 
-let persons = [{
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
 ];
 
+const generateRandId = () => {
+  return Math.floor(Math.random() * 1000 + 1);
+};
+
 app.get("/info", (req, res) => {
-    res.send(
-        `<p>Phonebook has info for ${persons.length} people</p> 
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p> 
     <p>${new Date()}</p>`
-    );
+  );
 });
 
 app.get("/api/persons", (req, res) => {
-    res.json(persons);
+  res.json(persons);
 });
 
 app.get("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const person = persons.find((p) => p.id === id);
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
 
-    person ? res.json(person) : res.status(404).send(`Resource ${id} not found`);
+  person ? res.json(person) : res.status(400).json({ "unknown id": id });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id);
-    persons = persons.filter((p) => p.id !== id);
-    console.log("deleted person", id);
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+  console.log("deleted person", id);
 
-    res.status(204).end();
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const data = req.body;
+
+  if (!data.name) {
+    return res.status(400).json({ error: "missing name" });
+  } else if (!data.number) {
+    console.log("missing number");
+    return res.status(400).json({ error: "missing number" });
+  } else if (persons.some((p) => p.name === data.name)) {
+    console.log("name must be unique");
+    return res.status(400).json({ error: "name must be unique" });
+  }
+  console.log(data);
+  const person = {
+    id: generateRandId(),
+    name: data.name,
+    number: data.number,
+  };
+  persons = persons.concat(person);
+
+  res.json(person);
 });
 
 const PORT = 3001;
