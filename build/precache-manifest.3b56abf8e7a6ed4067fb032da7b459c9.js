dee0aaf404f71f0469f8c24411104f1515cab322
Tamanji Che <cheambetamanji@gmail.com>
2020-06-16 13:24:37 +0300

    uibuild

---
+++ build/precache-manifest.3b56abf8e7a6ed4067fb032da7b459c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37b3f75241d3aac2a8c9880ebfcd7be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c952700e06da0ac71b06",
+    "url": "/static/js/2.f07a13ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f07a13ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2834c7f886c1c31c9a3",
+    "url": "/static/js/main.5a091959.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
