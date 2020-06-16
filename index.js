80c11eb532b08ef0ac962b5eb2794e7476ed75c5
Tamanji Che <cheambetamanji@gmail.com>
2020-06-16 13:03:28 +0300

    uibuild

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ morgan.token("data", (req, res) => {
 });
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+app.use(express.static("build"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
-
 
 let persons = [
   {
