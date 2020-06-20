ae9235ebfdc8d82cf6164be7d757142de5b71b95
Tamanji Che <cheambetamanji@gmail.com>
2020-06-20 13:46:56 +0300

    exer 3.15 - 3.18: get info, update, getById routes modified to use the database - errorHandler middleware implemented for the app

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-const Contact = require('./models/contact')
+const Contact = require("./models/contact");
 
 morgan.token("data", (req, res) => {
   return req.method === "POST" || req.method === "PUT"
@@ -19,52 +19,94 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 
+const missingFieldsErr = (data) => {
+  if (!data.name) throw new Error("missing name");
+  else if (!data.number) throw new Error("missing number");
+};
+
 app.get("/info", (req, res) => {
-  res.send(
-    `<p>Phonebook has info for ${persons.length} people</p> 
+  Contact.where({})
+    .countDocuments()
+    .then((count) =>
+      res.send(
+        `<p>Phonebook has info for ${count} people</p> 
     <p>${new Date()}</p>`
-  );
+      )
+    );
 });
 
 app.get("/api/persons", (req, res) => {
-  Contact.find({})
-  .then( contacts => res.json(contacts) )
+  Contact.find({}).then((contacts) =>
+    res.json(contacts.map((contact) => contact.toJSON()))
+  );
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((p) => p.id === id);
-
-  person ? res.json(person) : res.status(400).json({ "unknown id": id });
+app.get("/api/persons/:id", (req, res, next) => {
+  Contact.findById(req.params.id)
+    .then((contact) => {
+      contact ? res.json(contact.toJSON()) : res.status(404).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   Contact.findByIdAndRemove(req.params.id)
-  .then( deleted => res.status(204).end())
-  .catch( error => next(error))
+    .then((deleted) => res.status(204).end())
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
+app.put("/api/persons/:id", (req, res, next) => {
   const data = req.body;
 
-  if (!data.name) {
-    return res.status(400).json({ error: "missing name" });
-  } else if (!data.number) {
-    console.log("missing number");
-    return res.status(400).json({ error: "missing number" });
-  }
+  missingFieldsErr(data);
+
+  const contact = {
+    name: data.name,
+    number: data.number,
+  };
+
+  Contact.findByIdAndUpdate(req.params.id, contact, { new: true })
+    .then((updatedContact) =>
+      updatedContact ? res.json(updatedContact.toJSON()) : res.status(404).end()
+    )
+    .catch((error) => next(error));
+});
+
+app.post("/api/persons", (req, res, next) => {
+  const data = req.body;
+
+  missingFieldsErr(data);
 
   const contact = new Contact({
     name: data.name,
     number: data.number,
-  }) 
+  });
 
-  contact.save().then( savedContact => {
-    console.log('saved contact', savedContact);
-    res.json(savedContact)
-  })
-
+  contact.save().then((savedContact) => {
+    console.log("saved contact", savedContact);
+    res.json(savedContact.toJSON());
+  });
 });
+
+const unrecognisedEnpoint = (req, res, next) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, req, res, next) => {
+  console.error(`error: ${error.name}\t${error.message}`);
+  const err = res.status(400);
+  if (error.name === "CastError") {
+    return err.send({ error: "wrongly formatted id" });
+  } else if (error.message === "missing name") {
+    return err.json({ error: "missing name" });
+  } else if (error.message === "missing number") {
+    return err.json({ error: "missing number" });
+  }
+  next(error);
+};
+
+app.use(unrecognisedEnpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
